88fa449ed0967c0772426daf66263557d31d4f7e
Gino Contestabile <gino.contestabile@gmail.com>
2025-08-26 03:59:29 +0200

    Add: 747

--- dominant-747.c
+++ dominant-747.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+#include <limits.h>
 
 int dominantIndex(int* nums, int numsSize) {
-    
+    int currentMaximum = INT_MIN; 
+    int idxMax = -1;
+    for(int i = 0; i<numsSize; i++) { 
+        if(nums[i] > currentMaximum) {
+            currentMaximum = nums[i];
+            idxMax = i;
+        }
+    }
+    for(int i = 0; i<numsSize; i++) { 
+        if(i != idxMax && 2*nums[i] > currentMaximum)
+            return -1; 
+    }
+    return idxMax;  
 }
 
 int main(int argc, char** argv) {
-
-    
+    return 0;  
 }
